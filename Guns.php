452ce6bad807844e90677ef5bceb47fd675d4a85
Lenix31 <domantasgricukas@github.com>
2020-08-18 18:08:34 +0300

    checking

--- Guns.php
+++
@@ -1,4 +0,0 @@
-<?php include "Navbar.php" ?>
-<h1>I meant water guns what did you thought?</h1>
-<img class="img-full"  src="https://i.ibb.co/q16m249/images-2.jpg" alt="water gun">
-<p>You should find one of these at your local toy store</p>
--- coke.php
+++
@@ -1,4 +0,0 @@
-<?php include "Navbar.php" ?>
-<h1>You meant Coca Cola right?</h1>
-<img class="img-full" class="img-fluid" src="https://i.ibb.co/q0Lw7FR/images-1.jpg" alt="Coca Cola">
-<p>Go to your local store to buy it!</p>
