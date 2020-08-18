bab06c4325a738dc84026ea68a9b0326c8d18128
Lenix31 <domantasgricukas@github.com>
2020-08-18 18:08:57 +0300

    chacking

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include "Navbar.php" ?>
+<div > 
+    <div style="background-color: grey; color: white; " class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>This page is for robbers or other cirminals only</strong> for legal reasons this page is a joke.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div>
+        <h1>Welcome to the darkest place on the internet!</h1>
+
+        <p class="margin-top">In this site you <strong> can </strong> find anything what you think is illegal</p>
+        <p>We don't ask any quostions we just sell all the shit you need</p>
+        
+
+    </div>
+</div>
